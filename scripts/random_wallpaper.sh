15dfdae4378baddd022cc7b4ba1035fddaf235ac
Cristyle Garrard <29622724+CristylePGarrard@users.noreply.github.com>
2026-01-21 14:58:54 -0700

    Create random_wallpaper.sh
    selects random image and sets background

---
+++ scripts/random_wallpaper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# -------------------------------------------------- #
+# Script randomly chooses and image from a directory
+# and then updates the background with the img. 
+# -------------------------------------------------- #
+
+# --- config ---
+WALLPAPER_DIR="/path/to/directory"
+
+IMAGE=${find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" \) | shuf -n 1)
+
+pcmanfm --set-wallpaper="$IMAGE"
