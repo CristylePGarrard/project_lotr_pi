c6634f5a9c4b38bddfe89bdc4d9ce8f872e70083
Cristyle Garrard <29622724+CristylePGarrard@users.noreply.github.com>
2026-01-21 14:39:31 -0700

    Create wallpaperTrick.py
    runs a shell script that changes background at a random interval

---
+++ scripts/wallpaperTrick.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# This script selects a random number within a specified range
+# If the number == 1 a shell script is ran. 
+# The shell script changes the background to a random img from a dir
+
+import os
+import random
+import subprocess
+
+# path to img directory
+script_path = "path/to/script.sh"
+
+# function to choose random number within a range
+def random_number(min_num, max_num):
+  num_array = range(min_num, max_num)
+  rand_value = random.choice(num_array)
+  return rand_value
+
+rand_num = random_number(0,3)
+
+if rand_num == 1:
+  try:
+    result = subprocess.run(script_path, capture_output=True,
+                            text=True, shell=False)
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
+    print("Exit Code:" result.returncode)
+  except subprocess.CalledProcessError as e:
+    print(f"Script failed with exit code {e.returncode}"}
+    print("STDOUT:", e.stdout)
+    print("STDERR:", e.stderr)
+  except FileNotFoundError:
+    print(f"Error: The script file was not found at {script_path}")
+else:
+  print(f"Number is {str(rand_num)}")
